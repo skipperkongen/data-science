331a0b3acd068eb6e052cdd10b8dc8c8f8be80ba
Pimin Konstantin Kefaloukos <kostas@userreport.com>
2015-11-16 13:54:26 +0100

    get requests implemented with one caveat: if namespace/feature not in obfuscation map the values get dropped from output

--- kaggle/whats-cooking/reformat-submission.py
+++ kaggle/whats-cooking/reformat-submission.py
@@ -1,14 +1,21 @@
 import json
+import glob
 
-pathIn = "submission.json/part-r-00000-9472bd00-68a1-4da3-a134-94e77efbfd37"
-pathOut = "submission.csv"
+dirIn = "submission.json/*"
+fileOut = "submission.csv"
 
-with open(pathOut, 'w') as fout:
-    fout.write("id,cuisine\n")
-    with open(pathIn) as fin:
-        for line in fin:
-            data = json.loads(line)
-            fout.write("{0},{1}\n".format(data['id'], data['cuisine']))
+
+for pathIn in glob.glob(dirIn):
+    print pathIn
+    if "_SUCCESS" in pathIn:
+        continue
+
+    with open(fileOut, 'w') as fout:
+        fout.write("id,cuisine\n")
+        with open(pathIn) as fin:
+            for line in fin:
+                data = json.loads(line)
+                fout.write("{0},{1}\n".format(data['id'], data['cuisine']))
 
 
 
